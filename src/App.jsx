6dbcf2d6bc7647a668c20d09056379415ebd6e15
axundra <axundra@hotmail.com>
2024-04-18 23:36:27 -0300

    Ya está(creo)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Msj from './components/ItemListContainer'
+import Lista from './components/ItemListContainer'
 import Nav from './components/NavBar'
 
 function App() {
   return (
     <body>
-      <Nav />
-      <Msj mensaje={'Bienvenido a Salchiflix!'} />
+      <Nav /> 
+      <Lista titulo={"Shrek"} desc={"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum ex ratione esse labore aut accusamus corrupti nesciunt provident totam modi?"}/>
+      <Lista titulo={"Bee Movie"} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ipsum perspiciatis quidem delectus, corporis cupiditate voluptatum voluptatibus natus dolorum laborum dolor perferendis laboriosam culpa nemo soluta dolores, odio velit quia!"}/>
+      <Lista titulo={"Eterno resplandor de una mente sin recuerdos"} desc={"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt cupiditate impedit perspiciatis ipsum autem expedita accusamus labore fugit doloribus quasi. Aliquid inventore alias aut dolor."}/>
     </body>
   )
 }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,7 +1,11 @@
-function Msj({mensaje}) {
-    <main>
-        <p>{mensaje}</p>
-    </main>
+function Lista({ titulo, desc }) {
+    return (
+        <main>
+            <h2>{titulo}</h2>
+            <p>{desc}</p>
+            <br />
+            <p>_______________________________________________</p>
+        </main>)
 }
 
-export default Msj
+export default Lista
