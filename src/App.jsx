23e46a8bf71f40c8f50bdbdf7b66a55eb6d46007
axundra <axundra@hotmail.com>
2024-05-07 21:50:31 -0300

    le agregué los routes, faltan mas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import Lista from './components/ItemListContainer'
-import Nav from './components/NavBar'
+import { Nav } from './components/NavBar'
+import { PeliID, PeliLista, PelisCat } from './components/Layout'
+import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom'
 
 function App() {
-  return (
-    <body>
-      <Nav /> 
-      <Lista titulo={"Shrek"} desc={"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum ex ratione esse labore aut accusamus corrupti nesciunt provident totam modi?"}/>
-      <Lista titulo={"Bee Movie"} desc={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ipsum perspiciatis quidem delectus, corporis cupiditate voluptatum voluptatibus natus dolorum laborum dolor perferendis laboriosam culpa nemo soluta dolores, odio velit quia!"}/>
-      <Lista titulo={"Eterno resplandor de una mente sin recuerdos"} desc={"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt cupiditate impedit perspiciatis ipsum autem expedita accusamus labore fugit doloribus quasi. Aliquid inventore alias aut dolor."}/>
-    </body>
-  )
+  return <BrowserRouter>
+    <Nav />
+    <Routes>
+      <Route path='/' element={<PeliLista />} />
+      <Route path='/pelicula/:peliID' element={<PeliID />} />
+      <Route path='/categoria/:peliCat' element={<PelisCat />} />
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App
